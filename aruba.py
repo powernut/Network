93d4ab5b48a93801c2f34beeeed42c38329b979d
Huanwei Liu <huanwei.liu@gmail.com>
2020-02-11 20:25:31 +0800

    correct typo error

--- aruba.py
+++ aruba.py
@@ -3,15 +3,21 @@ import re
 import threading
 import time
 
-os_ver = '6.5.4.15'
+os_ver = '6.5.4.15'  # AP's OS version need to be same as this one
 netmiko_exceptions = (netmiko.ssh_exception.NetMikoTimeoutException,
                       netmiko.ssh_exception.NetMikoAuthenticationException)
 Qlock = threading.Lock()
+
+
+# To make print not interrupt by other thread
 def print_info(msg):
     Qlock.acquire()
     print(msg)
     Qlock.release()
+
+
 '''
+# This is another way to handle continue process of command.
 def handle_continue(net_connect, cmd):
     net_connect.remote_conn.sendall(cmd)
     time.sleep(1)
@@ -24,66 +30,74 @@ def handle_continue(net_connect, cmd):
         net_connect.disconnect()
         return output
 '''
-def handle_continue(net_connect,cmd):
+
+
+def handle_continue(net_connect, cmd):
     # send_command_timing as the router prompt is not returned
     output = net_connect.send_command_timing(cmd, strip_command=False, strip_prompt=False)
     if "y/n" in output:
         output += net_connect.send_command_timing("y\n", strip_command=False, strip_prompt=False)
     return output
 
-def need_upgrade(sh_ver,mac):
-    ap_ver = re.findall("Version (.*)" ,sh_ver)[0]
-    print_info ("AP:{} version:{}".format(mac,ap_ver))
+
+def need_upgrade(sh_ver, mac):
+    ap_ver = re.findall("Version (.*)", sh_ver)[0]
+    print_info("AP:{} version:{}".format(mac, ap_ver))
     return False if ap_ver.strip() == os_ver else True
+
 
 def upgrade_os(net_connect):
     output = net_connect.send_command('upgrade-image tftp://192.168.20.2/AlcatelInstant_Pegasus_6.5.4.15_73677')
     net_connect.disconnect()
     return output
 
-def config_ap(net_connect,mac):
+
+def config_ap(net_connect, mac):
     cmd = "copy tftp 192.168.20.2 backup0129.cfg system config"
-    #print("Download AP configuration \n" + cmd)
+    # print("Download AP configuration \n" + cmd)
 
     output = net_connect.send_command(cmd)
-    #print(output)
+    # print(output)
     if 'please reboot the AP' in output:
         print_info("Will reload the AP:" + mac)
-        output = handle_continue (net_connect,'reload\n')
+        output = handle_continue(net_connect, 'reload\n')
         net_connect.disconnect()
-        output = 'Successfully upload configuration, will reload AP:' + mac + '\nMessage:' +output
+        output = 'Successfully upload configuration, will reload AP:' + mac + '\nMessage:' + output
         return output
     else:
-        output = 'Fail to copy configuration to AP:' + mac +'\nMessage: '+ output
+        output = 'Fail to copy configuration to AP:' + mac + '\nMessage: ' + output
         return output
+
 
 def connect_ssh(host):
     net_connect = netmiko.ConnectHandler(**host)
     print_info("Connected to AP:{}".format(net_connect.find_prompt()))
     return net_connect
 
+
 def check_status(net_connect):
     pass
 
-def run_cmd(device,APs):
+
+def run_cmd(device, aps):
     mac = device.pop('AP')
-    #Q.put(device)
+    # Q.put(device)
     print_info("----------Start to Set AP:" + mac)
 
     print_info("Reset to Factory default setting for AP:" + mac)
     try:
         device["password"] = 'switch'
         connection = connect_ssh(device)
-        #Factory Reset
-        output = handle_continue(connection,"write erase all reboot\n")
-        print_info( 'AP: ' + mac + output)
+        # Factory Reset
+        output = handle_continue(connection, "write erase all reboot\n")
+        print_info('AP: ' + mac + output)
         if 'y/n' in output:
             print_info('AP: ' + mac + ' passed factory reset step')
             time.sleep(500)
-            APs.loc[mac, 'Factory_Reset'] = 'OK'
+            aps.loc[mac, 'Factory_Reset'] = 'OK'
         else:
             print_info('Failed to reset AP:' + mac)
-            APs.loc[mac, 'Factory_Reset'] = 'Failed'
+            aps.loc[mac, 'Factory_Reset'] = 'Failed'
     except netmiko_exceptions as ex:
         print('Failed to factory reset ', mac, ex)
         return
@@ -91,34 +105,34 @@ def run_cmd(device,APs):
     print_info("upgrade step for AP:" + mac)
     try:
         connection = connect_ssh(device)
-        #Upgrade if os version is different
+        # Upgrade if os version is different
         output = connection.send_command('show version')
-        #print(output)
-        if need_upgrade(output,mac) :
+        # print(output)
+        if need_upgrade(output, mac):
             print_info("Start to upgrade AP: " + mac)
             upgrade_os(connection)
             time.sleep(500)
         else:
             print_info("No need to upgrade AP: " + mac)
         print_info('AP: ' + mac + ' passed upgrade step')
-        APs.loc[mac,'Upgrade'] = 'OK'
+        aps.loc[mac, 'Upgrade'] = 'OK'
     except netmiko_exceptions as ex:
         print('Failed to Upgrade', mac, ex)
         return
-    
+
     print_info("Upload configuration step for AP: " + mac)
     try:
         connection = connect_ssh(device)
-        output = config_ap(connection,mac)
+        output = config_ap(connection, mac)
         print_info(output)
         if 'Successfully upload configuration' in output:
             time.sleep(500)
-            APs.loc[mac, 'Configuration'] = 'OK'
+            aps.loc[mac, 'Configuration'] = 'OK'
         else:
-            AP.loc[mac, 'Configuration'] = 'Failed'
+            aps.loc[mac, 'Configuration'] = 'Failed'
 
     except netmiko_exceptions as ex:
-        print('Failed to upload configuration ',mac,ex)
+        print('Failed to upload configuration ', mac, ex)
 
     print_info('Check AP:' + mac + 'status')
     try:
@@ -126,7 +140,7 @@ def run_cmd(device,APs):
         check_status(connection)
 
     except netmiko_exceptions as ex:
-        print("Failed to Check AP ",mac,ex)
+        print("Failed to Check AP ", mac, ex)
 
-    #Q.get(host)
-    #Q.task_done()
+    # Q.get(host)
+    # Q.task_done()
--- setupAP.py
+++ setupAP.py
@@ -1,37 +1,28 @@
-import os
 import threading
 import queue
-import re
 import time
 import pandas as pd
 import aruba
 
-
-
 thread_num = 100
 
-APs = pd.read_excel('data/devices.xlsx',index_col=0)
+APs = pd.read_excel('data/devices.xlsx', index_col=0)
 
-#Q = queue.Queue(thread_num)
+# Q = queue.Queue(thread_num)
 
-
-
-
-if __name__=='__main__':
-    #if os.path.exists('fail.txt') :
-    #   os.remove('fail.txt')
+if __name__ == '__main__':
+    # if os.path.exists('fail.txt') :
+    # os.remove('fail.txt')
     APs = pd.read_excel('data/devices.xlsx')
     devices = APs.drop(['Old_Version', 'Factory_Reset', 'Upgrade', 'Configuration'], axis=1).to_dict(orient='records')
-    APs.set_index('AP',inplace=True)
+    APs.set_index('AP', inplace=True)
 
     t0 = time.time()
 
     print('----------Setup AP is starting----------')
-
-
     threads = []
     for device in devices:
-        t = threading.Thread(target=aruba.run_cmd,args=(device,APs))
+        t = threading.Thread(target=aruba.run_cmd, args=(device, APs))
         t.setDaemon(True)
         t.start()
         threads.append(t)
@@ -39,11 +30,11 @@ if __name__=='__main__':
     for thread in threads:
         print(thread)
         thread.join()
- #   Q.join()
+    # Q.join()
 
     t1 = time.time()
 
-    filename = 'data/result_' + time.strftime("%Y%m%d%H%M%S",time.localtime(t1)) +'.xlsx'
+    filename = 'data/result_' + time.strftime("%Y%m%d%H%M%S", time.localtime(t1)) + '.xlsx'
     APs.to_excel(filename)
 
     print('----------Setup AP is finished----------')
